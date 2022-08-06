0b116296dff7b78e653cc7413d40e14e49087c6d
Hugo Ribeiro <hugo.ribeiro@inmetrics.com.br>
2022-08-06 15:08:57 -0300

    Initial commit

---
+++ src/main/java/one/digital/Parking/CloldParkingApplication.java
@@ -0,0 +1,13 @@
+package one.digital.Parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloldParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloldParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digital/Parking/CloldParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digital.Parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloldParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
